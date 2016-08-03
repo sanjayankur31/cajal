400eb70c199be3bb3ea40f2e32f3b9b9605e8699
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2016-08-03 19:57:16 +0100

    Initial commit

---
+++ src/populate.js
@@ -0,0 +1,20 @@
+var map;
+function init() {
+    map = new ol.Map({
+        controls: ol.control.defaults({
+            attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+                collapsible: true
+            })
+        }),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        target: 'map',
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+}
